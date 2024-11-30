70254f4c7d121426cd278e8feeb92000ab5e3c77
Priyanshuishwar <140679451+Priyanshuishwar@users.noreply.github.com>
2024-11-30 16:53:47 +0530

    Add files via upload

---
+++ ptack.java
@@ -0,0 +1,59 @@
+class ptack {
+    static final int max=100;
+    int top;
+    int a[]=new int[max];
+    boolean isEmpty(){
+        return (top<0);
+    }
+    ptack(){
+        top =-1;
+    }
+    boolean push(int x){
+        if(top>=max-1){
+            System.out.print("Stack Overflow");
+            return false;
+        }
+        else{
+            a[++top]=x;
+            System.out.println(x+"Pushed in Stack");
+            return true;
+        }
+    }
+     int pop(){
+        if(top<0){
+            System.out.print("Stack Underflow");
+            return 0;
+        }
+        else{
+            int x=a[top--];
+            return x;
+        }
+    }
+    int peek(){
+        if(top<0){
+            System.out.println("stack Underflow");
+            return 0;
+        }
+        else{
+            int x=a[top];
+            return x;
+        }
+    }
+    void print(){
+        for(int i=top;i>-1;i++){
+            System.out.print(""+a[i]);
+        }
+    }
+}
+
+public class stack {
+    public static void main(String[] args) {
+        ptack s=new ptack();
+        s.push(10);
+        s.push(20);
+        System.out.println(s.pop()+"Poped from Stack");
+    }
+    
+}
+    
+
